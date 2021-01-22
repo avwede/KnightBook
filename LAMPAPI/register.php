3fd15b289de81649fef01288c0aa9b7ff162d971
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-01-22 15:31:46 -0500

    started on register endpoint

---
+++ LAMPAPI/register.php
@@ -0,0 +1,73 @@
+<?php
+
+// unpack JSON object
+$inData = getRequestInfo();
+
+// make connection with database
+// TODO: figure out what the arguements need to be
+$conn = new mysqli("", "", "", "");
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    // check if username already exists, if it does tell user to try again
+    $sql = "SELECT ID FROM Users where Login='" . $inData["login"] . "'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) 
+    {
+        returnWithError("Username Has Been Taken");
+    }
+    // if username does not exist, create account and log them in
+    else 
+    {
+        $sql = "INSERT INTO Users (Login, Password, FirstName, LastName) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        // FIXME: these are just placeholder names for the db and json package for now, they can be changed if needed
+        $stmt->bind_param($inData["login"], $inData["password"], $inData["firstName"], $inData["lastName"]);
+        $stmt->execute();
+
+        $sql = "SELECT ID,firstName,lastName FROM Users where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $firstName = $row["firstName"];
+            $lastName = $row["lastName"];
+            $id = $row["ID"];
+            
+            returnWithInfo($firstName, $lastName, $id );
+        }
+        else
+        {
+            returnWithError( "Unexpected Error: Could not get login information from registration" );
+        }
+    }
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function returnWithInfo( $firstName, $lastName, $id )
+{
+    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
