9db33010e7ef83b568fadbb4672dff6c64f6be3b
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-01-23 21:10:22 -0500

    started logic to search for contacts. Returns all contacts where first name or last name is similar to the search

---
+++ LAMPAPI/searchContact.php
@@ -0,0 +1,56 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("", "", "", "");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$sql = "SELECT (FirstName, LastName) from Contacts WHERE FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "' and UserID=" . $inData["userId"];
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			while ($row = $result->fetch_assoc())
+			{
+				if ($searchCount > 0)
+				{
+					$searchResults .= ",";
+				}
+				$searchCount += 1;
+				// FIXME: not actually sure if this is right
+				$searchResults .= '"' . $row["FirstName"] . ' ' . $row["LastName"] . '"';
+			}
+		}
+		else
+		{
+			returnWithError("No Contacts Found");
+		}
+		$conn->close();
+	}
+
+	returnWithInfo($searchResults);
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
