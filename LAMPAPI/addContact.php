76699b7790f5372126264328eb2cf6633e9bf323
Ashley <avwede@gmail.com>
2021-01-26 13:53:45 -0500

    wrote addContact.php script

--- LAMPAPI/addContact.php
+++ LAMPAPI/addContact.php
@@ -4,22 +4,30 @@
 
 	// connect to db
 	$conn = new mysqli("localhost", "Group25", "25!!Poos", "KnightBook");
+	
 	if ($conn->connect_error)
 	{
 		returnWithError($conn->connect_error);
 	}
 	else
 	{
-		$sql = "INSERT INTO Contacts (FirstName, LastName, Email, Phone) VALUES (?, ?, ?, ?)";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param($inData["firstName"], $inData["lastName"], $inData["email"], $inData["phone"]);
-		$stmt->execute();
+		$sql = "INSERT INTO Contacts (UserID, FirstName, LastName, Email, Phone) VALUES (" . $inData["userId"] . ",'" . $inData["firstName"] . "','" . $inData["lastName"] . "','" . $inData["email"] . "','" . $inData["phone"] . "')";
+
+		if ($conn->query($sql) === FALSE)
+		{
+			returnWithError("Could not add contact.");
+		}
+		else
+		{
+			returnWithInfo("Successfully created the contact.");
+		}
+		
 		$conn->close();
 	}
 
 	function returnWithError($err)
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
@@ -32,6 +40,12 @@
 	{
 		header('Content-type: application/json');
 		echo $obj;
+	}
+
+	function returnWithInfo( $message )
+	{
+		$retValue = '{"message":' . $message . '"}';
+		sendResultInfoAsJson( $message );
 	}
 
 ?>
