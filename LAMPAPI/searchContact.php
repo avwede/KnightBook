9326bcebd81d7c5f9e954a2d4be57aa3c47a9477
Ashley <avwede@gmail.com>
2021-01-26 14:55:35 -0500

    removing userID check in searchContact

--- LAMPAPI/searchContact.php
+++ LAMPAPI/searchContact.php
@@ -13,9 +13,7 @@
 	}
 	else
 	{
-		$sql = "SELECT (FirstName, LastName) from Contacts WHERE (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%') and UserID=" . $inData["userId"];
-
-		// SELECT (FirstName, LastName) from Contacts WHERE (FirstName like '%s%' or LastName like '%s%') and UserID=2
+		$sql = "SELECT (FirstName, LastName) from Contacts WHERE (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%')";
 		
 		$result = $conn->query($sql);
 		
