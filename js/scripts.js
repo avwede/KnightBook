4bf95b6d7292b428b5c9001582d167b4b72a2a9e
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2021-01-26 22:19:21 -0500

    Created most of front end design.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,76 @@
+var urlBase = 'http://knightbook.rocks/LAMPAPI';
+var extension = 'php';
 
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+function doLogin()
+{
+
+}
+
+function doRegister()
+{
+
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").textContent = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function searchContacts()
+{
+
+}
+
+function addContact()
+{
+	
+}
