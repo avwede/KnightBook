9fbac702050f661b3f975c6bfb8230d4e69f94c7
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2021-01-29 21:15:27 -0500

    Added example of contact table structure and updated JS accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,38 @@ function doLogin()
 
 function doRegister()
 {
+<<<<<<< Updated upstream
+=======
+	// fields that user will enter into register page
+	var firstName = document.getElementById("registerFirstName").value;
+	var lastName = document.getElementById("registerLastName").value;
+	var login = document.getElementById("registerName").value;
+	var password = document.getElementById("registerPassword").value;
+	var hash = md5( password );
+	
+	// used to display to user return result of login attempt
+	document.getElementById("registerResult").innerHTML = "";
+
+	// create json object for backend
+	var jsonPayload = '{"firstName" : "' + firstName + '","lastName" : "' + lastName + '","login" : "' + login + '", "password" : "' + hash + '"}';
+	var url = urlBase + '/register.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		// send payload to register api
+		xhr.send(jsonPayload);
+		
+		// get response from api
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		// save values from api response
+		userId = jsonObject.id;
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+>>>>>>> Stashed changes
 
 }
 
@@ -67,10 +99,97 @@ function doLogout()
 
 function searchContacts()
 {
+<<<<<<< Updated upstream
+=======
+	var srch = document.getElementById("searchText").value;
+	document.getElementById("searchResult").innerHTML = "";
+	
+	// result for later
+	var nameList = "";
+	// var emailList = "";
+	// var phoneList = "";
+	// var majorList = "";
+	// var lastOnlineList = "";
+	
+	// make json payload and send to api
+	var jsonPayload = `{ "search" : "${srch}", "userId" : ${userId} }`;
+	var url = urlBase + '/searchContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				var jsonObject = JSON.parse( xhr.responseText );
+				
+				for(let i=0; i<jsonObject.results.length; i++ )
+				{
+					nameList += "<tr>"
+					for (let j=0; j<jsonObject.results[i].length; j++)
+					{
+						nameList += "<td>" + jsonObject.results[i][j] + "</td>";
+					}
+					nameList += "</tr>"
+				}
+				
+				let table = document.getElementById("contactHeader");
+				table.insertAdjacentHTML("afterend", nameList);
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("searchResult").innerHTML = err.message;
+	}
+}
+>>>>>>> Stashed changes
 
 }
 
 function addContact()
 {
 	
+<<<<<<< Updated upstream
+=======
+	// TODO: add major functionality (var declared above) if there is time
+	var jsonPayload = 	`{ "firstName" : "${firstName}", "lastName" : "${lastName}", 
+						"email" : "${email}", "phone" : "${phone}" }`;
+	var url = urlBase + '/addContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("addContactResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("addContactResult").innerHTML = err.message;
+	}
+}
+
+// TODO: fill in body
+function updateContact() 
+{
+
+}
+
+// TODO: fill in body
+function deleteContact()
+{
+
+>>>>>>> Stashed changes
 }
