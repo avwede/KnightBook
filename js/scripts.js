cd329fa0cd59227a1763abc5af8e518fea7b345a
Prathik Ramesh <prathik.ramesh@knights.ucf.edu>
2021-01-29 21:26:36 -0500

    Eliminated conflict in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,57 @@ var lastName = "";
 
 function doLogin()
 {
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	var login = document.getElementById("loginName").value;
+	var password = document.getElementById("loginPassword").value;
+	var hash = md5( password );
+	
+	// used to display to user return result of login attempt
+	document.getElementById("loginResult").innerHTML = "";
 
+	// create json object for backend
+	// var jsonPayload = `{"login" : "${login}", "password" : "${hash}"}`;
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var url = urlBase + '/login.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "Username/Password combination incorrect";
+			return;
+		}
+		
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		saveCookie();
+	
+		// FIXME: this should redirect user to home page, whatever that file gets called
+		// just checking to make sure link up works
+		window.location.href = "contacts.html";
+	}
+	catch(err)
+	{
+		alert(err.message); // FIXME: get rid of this
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
 }
 
 function doRegister()
 {
-<<<<<<< Updated upstream
-=======
 	// fields that user will enter into register page
 	var firstName = document.getElementById("registerFirstName").value;
 	var lastName = document.getElementById("registerLastName").value;
@@ -43,8 +87,16 @@ function doRegister()
 		userId = jsonObject.id;
 		firstName = jsonObject.firstName;
 		lastName = jsonObject.lastName;
->>>>>>> Stashed changes
 
+		saveCookie();
+	
+		// FIXME: this should redirect user to home page, whatever that file gets called
+		window.location.href = "contacts.html";
+	}
+	catch(err)
+	{
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
 }
 
 function saveCookie()
@@ -99,8 +151,6 @@ function doLogout()
 
 function searchContacts()
 {
-<<<<<<< Updated upstream
-=======
 	var srch = document.getElementById("searchText").value;
 	document.getElementById("searchResult").innerHTML = "";
 	
@@ -147,15 +197,10 @@ function searchContacts()
 		document.getElementById("searchResult").innerHTML = err.message;
 	}
 }
->>>>>>> Stashed changes
 
-}
 
 function addContact()
 {
-	
-<<<<<<< Updated upstream
-=======
 	// TODO: add major functionality (var declared above) if there is time
 	var jsonPayload = 	`{ "firstName" : "${firstName}", "lastName" : "${lastName}", 
 						"email" : "${email}", "phone" : "${phone}" }`;
@@ -191,5 +236,4 @@ function updateContact()
 function deleteContact()
 {
 
->>>>>>> Stashed changes
 }
