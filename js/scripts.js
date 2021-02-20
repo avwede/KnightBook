06947bf6b5a98285a3b24fea3f8675c74f711f62
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-02-19 19:19:53 -0500

    removed debug from addContact()

--- js/scripts.js
+++ js/scripts.js
@@ -254,7 +254,6 @@ function addContact()
 				var jsonObject = JSON.parse(xhr.responseText);
 		
 				if (jsonObject.error !== "") {
-					alert(jsonObject.error);
 					document.getElementById("contactAddResult").innerHTML = jsonObject.error;
 					return;
 				}
