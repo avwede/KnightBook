f0024d6d3460374e76b7e54136ac835b42c2592f
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-02-21 01:28:09 -0500

    debugging

--- js/scripts.js
+++ js/scripts.js
@@ -275,6 +275,8 @@ function addContact()
 				person += `<td>${major}</td>`;
 				person += `<td class='buttons'><i class='far fa-edit modify-btn btn btn-defualt' onclick='updateContact(this);'></i><i class='fas fa-trash-alt modify-btn btn btn-default' onclick='deleteContact(this);'></i></td></tr>`;
 
+				if (elementIds.length == 0)
+					lastElement = "contactHeader";
 				let table = document.getElementById(lastElement);
 				table.insertAdjacentHTML("afterend", person);
 
