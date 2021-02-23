f27919b4a53e4e94c7b00d03c11d29dbc59479c2
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-02-22 19:35:49 -0500

    fixed error message on register

--- js/scripts.js
+++ js/scripts.js
@@ -97,21 +97,18 @@ function doRegister()
 	{
 		// send payload to register api
 		xhr.send(jsonPayload);
-		if (this.readyState == 4 && this.status == 200) 
-		{
-			// get response from api
-			var jsonObject = JSON.parse( xhr.responseText );
-			
-			// save values from api response
-			userId = jsonObject.id;
-			firstName = jsonObject.firstName;
-			lastName = jsonObject.lastName;
+		// get response from api
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		// save values from api response
+		userId = jsonObject.id;
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
 
-			if (userId == 0)
-				throw jsonObject.error;
+		if (userId == 0)
+			throw jsonObject.error;
 
-			doLogin(login, password, "registerResult");
-		}
+		doLogin(login, password, "registerResult");
 	}
 	catch(err)
 	{
